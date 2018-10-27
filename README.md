a2ae7c47ccf02ff91039054dc39d30c5072b1c72
Mark Bates <mark@markbates.com>
2018-10-27 08:42:42 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Packd
+
+This is a collection of interfaces designed to make using github.com/gobuffalo/packr easier, and to make the transition between v1 and v2 as seamless as possible
---
+++ box.go
@@ -0,0 +1,49 @@
+package packd
+
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
+type WalkFunc func(string, File) error
+
+type Box interface {
+	HTTPBox
+	Lister
+	Addable
+	Finder
+	Walkable
+	Has(string) bool
+}
+
+type Walkable interface {
+	Walk(wf WalkFunc) error
+}
+
+type Finder interface {
+	Find(string) ([]byte, error)
+	FindString(name string) (string, error)
+}
+
+type HTTPBox interface {
+	Open(name string) (http.File, error)
+}
+
+type Lister interface {
+	List() []string
+}
+
+type Addable interface {
+	AddString(path string, t string) error
+	AddBytes(path string, t []byte) error
+}
+
+type File interface {
+	io.ReadCloser
+	io.Writer
+	FileInfo() (os.FileInfo, error)
+	Readdir(count int) ([]os.FileInfo, error)
+	Seek(offset int64, whence int) (int64, error)
+	Stat() (os.FileInfo, error)
+}
